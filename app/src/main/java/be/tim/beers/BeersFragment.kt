d5b8de5c81d8932cf740414b93ab95711d81e4dc
Tim Van Ginderen <timvanginderen@gmail.com>
2020-11-05 14:20:17 +0100

    Add beers fragment and assign as start destination

---
+++ app/src/main/java/be/tim/beers/BeersFragment.kt
@@ -0,0 +1,18 @@
+package be.tim.beers
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class BeersFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.beers_frag, container, false)
+    }
+}
