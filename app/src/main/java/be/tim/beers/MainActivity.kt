2bab3af7c413e62f6899d59e9a283557c0d52598
Tim Van Ginderen <timvanginderen@gmail.com>
2020-11-06 21:56:11 +0100

    Connect navigation controller to app bar

--- app/src/main/java/be/tim/beers/MainActivity.kt
+++ app/src/main/java/be/tim/beers/MainActivity.kt
@@ -2,12 +2,48 @@ package be.tim.beers
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         setSupportActionBar(findViewById(R.id.toolbar))
+        setupNavigationDrawer()
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        val navController: NavController = findNavController(R.id.fragment)
+        appBarConfiguration =
+                // TODO: 06/11/2020 fix deprecated code
+                AppBarConfiguration.Builder(R.id.beersFragment)
+                        .setDrawerLayout(drawerLayout)
+                        .build()
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        findViewById<NavigationView>(R.id.nav_view)
+                .setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.fragment).navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+
+    private fun setupNavigationDrawer() {
+        drawerLayout = (findViewById<DrawerLayout>(R.id.drawer_layout))
+                .apply {
+                    setStatusBarBackground(R.color.brown_600)
+                }
     }
 }
