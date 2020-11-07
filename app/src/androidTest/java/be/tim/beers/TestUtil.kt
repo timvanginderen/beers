e5d4a2087b1007e14cef7f70740d5a61d83966ce
Tim Van Ginderen <timvanginderen@gmail.com>
2020-11-07 21:22:17 +0100

    Add navigation drawer instrumentation test

---
+++ app/src/androidTest/java/be/tim/beers/TestUtil.kt
@@ -0,0 +1,15 @@
+package be.tim.beers
+
+import android.app.Activity
+import androidx.appcompat.widget.Toolbar
+import androidx.test.core.app.ActivityScenario
+
+fun <T : Activity> ActivityScenario<T>.getToolbarNavigationContentDescription()
+        : String {
+    var description = ""
+    onActivity {
+        description =
+            it.findViewById<Toolbar>(R.id.toolbar).navigationContentDescription as String
+    }
+    return description
+}
